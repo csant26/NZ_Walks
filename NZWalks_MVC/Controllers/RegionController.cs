b597e0e16462acaa2d6f71d88f94590e39fcf82e
csant26 <shishantstha@gmail.com>
2024-09-21 08:51:45 +0545

    PUT

--- NZWalks_MVC/Controllers/RegionController.cs
+++ NZWalks_MVC/Controllers/RegionController.cs
@@ -1,23 +1,27 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NZWalks_MVC.Models;
 using NZWalks_MVC.Models.DTO;
+using System.Text;
+using System.Text.Json;
 
 namespace NZWalks_MVC.Controllers
 {
     public class RegionController : Controller
     {
         private readonly IHttpClientFactory _httpClientFactory;
-
+        private string url = "https://localhost:7218/api/Region";
         public RegionController(IHttpClientFactory httpClientFactory)
         {
             _httpClientFactory = httpClientFactory;
         }
+        [HttpGet]
         public async Task<IActionResult> Index()
         {
             List<RegionDTO> response = new List<RegionDTO>();
             try
             {
                 var client = _httpClientFactory.CreateClient();
-                var httpResponse = await client.GetAsync("https://localhost:7218/api/Region");
+                var httpResponse = await client.GetAsync(url);
 
                 httpResponse.EnsureSuccessStatusCode();
 
@@ -33,5 +37,37 @@ namespace NZWalks_MVC.Controllers
 
             return View(response);
         }
+
+        [HttpGet]
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddRegionViewModel addRegion)
+        {
+            var client = _httpClientFactory.CreateClient();
+            var httpRequestMessage = new HttpRequestMessage
+            {
+                Method = HttpMethod.Post,
+                RequestUri = new Uri(url),
+                Content = new StringContent(JsonSerializer.Serialize(addRegion),Encoding.UTF8,"application/json")
+            };
+            var httpResponseMessage = await client.SendAsync(httpRequestMessage);
+
+            httpResponseMessage.EnsureSuccessStatusCode();
+
+            var response = httpResponseMessage.Content.ReadFromJsonAsync<RegionDTO>();
+
+            if (response is not null)
+            {
+                return RedirectToAction("Index", "Region");
+            }
+
+            return View();
+
+        }
+
     }
-}
+}   
---
+++ NZWalks_MVC/Models/AddRegionViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NZWalks_MVC.Models
+{
+    public class AddRegionViewModel
+    {
+        public string Code { get; set; }
+        public string Name { get; set; }
+        public string? RegionImageUrl { get; set; }
+    }
+}
